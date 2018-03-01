6daa84893d0fa60819072abd4ebfb2964355ef5c
Gabriel Godoy <hi@gabrielgodoy.com>
2018-03-01 13:20:09 -0300

    fix: minor fixes

--- cypress/integration/change-products-number.js
+++ cypress/integration/change-products-number.js
@@ -1,15 +1,13 @@
 describe('Shelf of products', function() {
-  beforeEach(function() {
-    cy.visit('http://localhost:3000');
-  })
-
   it('Should change products numbers', function() {
+    cy.visit('http://localhost:3000');
     cy.get('.shelf-container').first().find('.btn-decrease').click();
     cy.get('.shelf-container').first().find('.btn-decrease').click();
     cy.get('.shelf-container').first().find('.product').should('have.length', 3);
   });
-
+  
   it('Should change to a minimum of one product in a shelf', function() {
+    cy.visit('http://localhost:3000');
     cy.get('.shelf-container').first().find('.btn-decrease').click();
     cy.get('.shelf-container').first().find('.btn-decrease').click();
     cy.get('.shelf-container').first().find('.btn-decrease').click();
